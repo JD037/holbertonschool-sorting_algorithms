f3f4e3e25c802e8761fc886222f784c25789a517
JD037 <joj237@gmail.com>
2023-04-21 13:45:39 -0700

    1.task 1st commit

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list using the Insertion sort
+ * algorithm
+ * @list: doubly linked list of integers
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current, *temp;
+
+    if (!list || !(*list) || !((*list)->next))
+        return;
+
+    current = (*list)->next;
+    while (current)
+    {
+        temp = current->next;
+        while (current->prev && current->prev->n > current->n)
+        {
+            current->prev->next = current->next;
+            if (current->next)
+                current->next->prev = current->prev;
+            current->next = current->prev;
+            current->prev = current->prev->prev;
+            current->next->prev = current;
+            if (current->prev)
+                current->prev->next = current;
+            else
+                *list = current;
+            print_list(*list);
+        }
+        current = temp;
+    }
+}
--- sort.h
+++ sort.h
@@ -2,8 +2,21 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void bubble_sort(int *array, size_t size);
+/* Data structure for doubly linked list */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
