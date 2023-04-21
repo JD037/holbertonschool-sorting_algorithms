0965f8ab7906a741e98d70227ce8bcfaf1762560
JD037 <joj237@gmail.com>
2023-04-21 13:54:47 -0700

    fixed weird unix/vsc issue

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,29 +7,29 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *current, *temp;
 
-    if (!list || !(*list) || !((*list)->next))
-        return;
+	if (!list || !(*list) || !((*list)->next))
+		return;
 
-    current = (*list)->next;
-    while (current)
-    {
-        temp = current->next;
-        while (current->prev && current->prev->n > current->n)
-        {
-            current->prev->next = current->next;
-            if (current->next)
-                current->next->prev = current->prev;
-            current->next = current->prev;
-            current->prev = current->prev->prev;
-            current->next->prev = current;
-            if (current->prev)
-                current->prev->next = current;
-            else
-                *list = current;
-            print_list(*list);
-        }
-        current = temp;
-    }
+	current = (*list)->next;
+	while (current)
+	{
+		temp = current->next;
+		while (current->prev && current->prev->n > current->n)
+		{
+			current->prev->next = current->next;
+			if (current->next)
+				current->next->prev = current->prev;
+			current->next = current->prev;
+			current->prev = current->prev->prev;
+			current->next->prev = current;
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
+			print_list(*list);
+		}
+		current = temp;
+	}
 }
